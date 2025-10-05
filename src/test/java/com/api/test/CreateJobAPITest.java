a888c0f5f816cae0165d8329ac46c495959dc57d
Manasi <Manasiavachat14@gmail.com>
2025-10-05 12:55:29 +0530

    DateTimeUtil Class:
    Created DateTimeUtil Class to provide us with a past ISO Standard UTC
    time

--- src/test/java/com/api/test/CreateJobAPITest.java
+++ src/test/java/com/api/test/CreateJobAPITest.java
@@ -1,6 +1,9 @@
 package com.api.test;
 
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.startsWith;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,14 +16,8 @@ import com.api.request.model.Customer;
 import com.api.request.model.CustomerAddress;
 import com.api.request.model.CustomerProduct;
 import com.api.request.model.Problems;
-import com.api.utils.AuthTokenProvider;
-import com.api.utils.ConfigManager;
+import static com.api.utils.DateTimeUtil.*;
 import com.api.utils.SpecUtil;
-
-import io.restassured.http.ContentType;
-import static io.restassured.module.jsv.JsonSchemaValidator.*;
-
-import static io.restassured.RestAssured.*;
 
 public class CreateJobAPITest {
 	
@@ -29,7 +26,7 @@ public class CreateJobAPITest {
 		//Creating the CreateJobPayload object
 				Customer custmer=new Customer("Manasi","Avachat","9767145100","","manasiavachat14@gmail.com","");
 				CustomerAddress customerAddress=new CustomerAddress("20B","Atria","HMTMain","HMT","Jalahalli","560089","India","Karnataka");
-				CustomerProduct customerProduct=new CustomerProduct("2025-04-07T18:30:00.000Z","66381664897457","66381664897457","66381664897457","2025-04-07T18:30:00.000Z",1,1);
+				CustomerProduct customerProduct=new CustomerProduct(getTimeWithDaysAgo(10),"99381664897457","99381664897457","99381664897457",getTimeWithDaysAgo(10),1,1);
 				Problems problems = new Problems(1, "Battery issue");
 				List<Problems> problemList=new ArrayList<Problems>();
 				problemList.add(problems);
