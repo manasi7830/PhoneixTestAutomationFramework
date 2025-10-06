16ed6ad57adc0c659205b67d7b809cd4fa1d72bb
Manasi <Manasiavachat14@gmail.com>
2025-10-06 16:34:55 +0530

    Introduced Enums for Handling the codes for the Create Job API Test. Reduced Magic Number Issue in the test script.

--- src/test/java/com/api/test/CreateJobAPITest.java
+++ src/test/java/com/api/test/CreateJobAPITest.java
@@ -1,5 +1,6 @@
 package com.api.test;
 
+import static com.api.utils.DateTimeUtil.getTimeWithDaysAgo;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
@@ -10,13 +11,19 @@ import java.util.List;
 
 import org.testng.annotations.Test;
 
+import com.api.constant.Model;
+import com.api.constant.OEM;
+import com.api.constant.Platform;
+import com.api.constant.Problem;
+import com.api.constant.Product;
 import com.api.constant.Role;
+import com.api.constant.ServiceLocation;
+import com.api.constant.Warranty_Status;
 import com.api.request.model.CreateJobPayload;
 import com.api.request.model.Customer;
 import com.api.request.model.CustomerAddress;
 import com.api.request.model.CustomerProduct;
 import com.api.request.model.Problems;
-import static com.api.utils.DateTimeUtil.*;
 import com.api.utils.SpecUtil;
 
 public class CreateJobAPITest {
@@ -26,12 +33,12 @@ public class CreateJobAPITest {
 		//Creating the CreateJobPayload object
 				Customer custmer=new Customer("Manasi","Avachat","9767145100","","manasiavachat14@gmail.com","");
 				CustomerAddress customerAddress=new CustomerAddress("20B","Atria","HMTMain","HMT","Jalahalli","560089","India","Karnataka");
-				CustomerProduct customerProduct=new CustomerProduct(getTimeWithDaysAgo(10),"99381664897457","99381664897457","99381664897457",getTimeWithDaysAgo(10),1,1);
-				Problems problems = new Problems(1, "Battery issue");
+				CustomerProduct customerProduct=new CustomerProduct(getTimeWithDaysAgo(10),"97381664897457","97381664897457","97381664897457",getTimeWithDaysAgo(10),Product.NEXUS_2.getCode(),Model.NEXUS_2_BLUE.getCode());
+				Problems problems = new Problems(Problem.POOR_BATTERY_LIFE.getCode(), "Battery issue");
 				List<Problems> problemList=new ArrayList<Problems>();
 				problemList.add(problems);
 				
-				CreateJobPayload createJobPayload=new CreateJobPayload(0, 2, 1, 1, custmer, customerAddress, customerProduct, problemList);
+				CreateJobPayload createJobPayload=new CreateJobPayload(ServiceLocation.SERVICE_LOCATION_A.getCode(), Platform.FRONT_DESK.getCode(), Warranty_Status.IN_WARRANTY.getCode(), OEM.GOOGLE.getCode(), custmer, customerAddress, customerProduct, problemList);
 		
 		given()
 		.spec(SpecUtil.requestSpecWithAuth(Role.FD, createJobPayload))
