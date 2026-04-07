954be7babca730f0ae36453b90000cda263c0290
Manasi <Manasiavachat14@gmail.com>
2026-04-07 17:45:46 +0530

    Update DatabaseManager to use Hikary CP library

--- src/test/java/com/database/DatabaseManager.java
+++ src/test/java/com/database/DatabaseManager.java
@@ -5,27 +5,62 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import com.api.utils.ConfigManager;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class DatabaseManager {
 
 	private static final String DB_URL = ConfigManager.getProperty("DB_URL");
 	private static final String DB_USERNAME = ConfigManager.getProperty("DB_USER_NAME");
 	private static final String DB_PASSWORD = ConfigManager.getProperty("DB_PASSWORD");
-	private volatile static Connection conn;
+	private static final int MAXIMUM_POOL_SIZE = Integer.parseInt(ConfigManager.getProperty("MAXIMUM_POOL_SIZE"));
+	private static final int MINIMUM_IDLE_COUNT = Integer.parseInt(ConfigManager.getProperty("MINIMUM_IDLE_COUNT"));
+	private static final int CONNECTION_TIMEOUT_SEC = Integer
+			.parseInt(ConfigManager.getProperty("CONNECTION_TIMEOUT_SEC"));
+	private static final int MAX_LIFE_TIME_IN_MINS = Integer
+			.parseInt(ConfigManager.getProperty("MAX_LIFE_TIME_IN_MINS"));
+	private static final int IDEL_TIMEOUT_SEC = Integer.parseInt(ConfigManager.getProperty("IDEL_TIMEOUT_SEC"));
+	private static final String HIKARI_CP_POOL_NAME = ConfigManager.getProperty("HIKARI_CP_POOL_NAME");
+
+	private static HikariConfig hikariConfig;
+	private volatile static HikariDataSource hikariDataSource;
 
 	private DatabaseManager() {
 
 	}
 
-	public synchronized static void createConnection() throws SQLException {
-		if (conn == null) {
+	private static void intializePool() {
+		if (hikariDataSource == null) {
 			synchronized (DatabaseManager.class) {
-				if (conn == null) {
-					conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-					System.out.println(conn);
+				if (hikariDataSource == null) {
+					hikariConfig = new HikariConfig();
+					hikariConfig.setJdbcUrl(DB_URL);
+					hikariConfig.setUsername(DB_USERNAME);
+					hikariConfig.setPassword(DB_PASSWORD);
+					hikariConfig.setMaximumPoolSize(MAXIMUM_POOL_SIZE);
+					hikariConfig.setMinimumIdle(MINIMUM_IDLE_COUNT);
+					hikariConfig.setConnectionTimeout(CONNECTION_TIMEOUT_SEC*1000);
+					hikariConfig.setIdleTimeout(IDEL_TIMEOUT_SEC * 1000);
+					hikariConfig.setMaxLifetime(MAX_LIFE_TIME_IN_MINS * 60 * 1000);
+					hikariConfig.setPoolName(HIKARI_CP_POOL_NAME);
+
+					hikariDataSource = new HikariDataSource(hikariConfig);
 				}
 
 			}
 		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		Connection connection = null;
+		if (hikariDataSource == null) {
+			intializePool();
+		} else if (hikariDataSource.isClosed()) {
+			throw new SQLException("HIKARI DATA SOURCE IS CLOSED");
+		}
+
+		connection = hikariDataSource.getConnection();
+
+		return connection;
 	}
 }
